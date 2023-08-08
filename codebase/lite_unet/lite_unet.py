978545f58d30a45c3ff1cf52a7a4febf11a7294f
Zhenglin-WSL <zhengli3@ualberta.ca>
2023-08-08 21:53:15 +0800

    mix precision

--- codebase/lite_unet/lite_unet.py
+++ codebase/lite_unet/lite_unet.py
@@ -7,14 +7,13 @@ import torch.nn as nn
 from torch.autograd import Variable
 from torch.utils.data import DataLoader
 from torchvision.utils import save_image
+from torch.cuda.amp import GradScaler, autocast
 
 from zhenglin.dl.networks.unet import UNet
 from zhenglin.dl.networks.discriminator import Discriminator
 from zhenglin.dl.utils import LinearLambdaLR
 
 from tqdm import tqdm
-# import wandb
-# wandb.init(project="sketch closer")
 
 from dataset import AnimeSketch
 from model import LightUNet, LighterUNet
@@ -29,7 +28,7 @@ parser.add_argument('--output_nc', type=int, default=1, help='number of channels
 parser.add_argument('--num_workers', type=int, default=4, help='number of cpu threads to use during batch generation')
 ### training args
 parser.add_argument('--start_epoch', type=int, default=0, help='starting epoch')
-parser.add_argument('--end_epoch', type=int, default=1000, help='number of epochs of training')
+parser.add_argument('--end_epoch', type=int, default=600, help='number of epochs of training')
 parser.add_argument('--decay_epoch', type=int, default=400, help='epoch to start linearly decaying the learning rate to 0')
 parser.add_argument('--batch_size', type=int, default=1, help='size of the batches')
 parser.add_argument('--lr', type=float, default=0.002, help='initial learning rate')
@@ -40,16 +39,14 @@ args = parser.parse_args()
 DEVICE = 0
 
 generator = LightUNet(1, 1, 1).to(DEVICE)
-# discriminator = Discriminator(3, 1).to(DEVICE)
 
 criterion_pixel = nn.MSELoss().to(DEVICE)
-# criterion_gan = nn.MSELoss().to(DEVICE)
 
 optimizer_G = torch.optim.Adam(generator.parameters(), lr=args.lr, betas=(0.9, 0.999))
-# optimizer_D = torch.optim.Adam(generator.parameters(), lr=args.lr, betas=(0.9, 0.999))
 
 lr_scheduler_G = torch.optim.lr_scheduler.LambdaLR(optimizer_G, lr_lambda=LinearLambdaLR(args.end_epoch, args.start_epoch, args.decay_epoch).step)
-# lr_scheduler_D = torch.optim.lr_scheduler.LambdaLR(optimizer_D, lr_lambda=LinearLambdaLR(args.end_epoch, args.start_epoch, args.decay_epoch).step)
+
+grad_scaler = GradScaler()
 
 Tensor = torch.cuda.FloatTensor
 batch_shape = (args.batch_size, args.input_nc, args.patch_size, args.patch_size)
@@ -60,7 +57,6 @@ dataset = AnimeSketch(args.dataroot, args.patch_size, color=False)
 dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=args.num_workers)
 
 generator.train()
-# discriminator.train()
 
 for epoch in tqdm(range(args.start_epoch, args.end_epoch + 1)):
     for i, batch in enumerate(dataloader):
@@ -74,15 +70,16 @@ for epoch in tqdm(range(args.start_epoch, args.end_epoch + 1)):
         
         ##### Generator #####
         optimizer_G.zero_grad()
-        
-        closed = generator(opened)
+        with autocast():
+            closed = generator(opened)
+            
+            loss_pixel = criterion_pixel(closed, gt)
+            
+            loss_G = loss_pixel * 10.0
 
-        loss_pixel = criterion_pixel(closed, gt)
-
-        loss_G = loss_pixel * 10.0
-        
-        loss_G.backward()
-        optimizer_G.step()
+        grad_scaler.scale(loss_G).backward()
+        grad_scaler.step(optimizer_G)
+        grad_scaler.update()
         
     lr_scheduler_G.step()
     
@@ -93,5 +90,4 @@ for epoch in tqdm(range(args.start_epoch, args.end_epoch + 1)):
         save_image(mask[0] * 255, f'imgs/{epoch}_mask.png')
         
     if epoch % 100 == 0:
-        torch.save(generator.state_dict(), f'models/LightUNet_generator_{epoch}.pth')
-        # torch.save(discriminator.state_dict(), f'models/discriminator_{epoch}.pth')
+        torch.save(generator.state_dict(), f'models/LightUNet_mix_generator_{epoch}.pth')
